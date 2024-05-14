c45e6c758881c56f3c6d37c98d7157665d93b079
adilaroisa <adilaroisa688@gmail.com>
2024-05-14 15:13:37 +0700

    membuat class mahasiswa

--- PemdasStaticMemberFunctionConstructorChain/PemdasStaticMemberFunctionConstructorChain.cpp
+++ PemdasStaticMemberFunctionConstructorChain/PemdasStaticMemberFunctionConstructorChain.cpp
@@ -2,6 +2,20 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa {
+
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	Mahasiswa(string pnama) :nama(pnama) {
+		setID();
+	}
+};
 
 
 
